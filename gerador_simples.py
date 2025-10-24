0dd2e8651dd56198782705cfd76aebf8d713ddb5
leolessi <leonardolessi@gmail.com>
2025-10-23 22:17:49 -0300

    rmv comentarios de gerador_simples.py e add links.txt

--- gerador_simples.py
+++ gerador_simples.py
@@ -1,11 +1,3 @@
-# Links:
-# - https://www.youtube.com/
-# - https://github.com/
-# - https://www.instagram.com/
-# - https://www.chess.com/
-# - https://portalhashtag.com/
-
-
 import qrcode
 
 imagem = qrcode.make("https://www.youtube.com/")
